1ee81ba11ce08ad5085c1b7af136edff08558c01
Kristof Vermeulen <kristofvermeulen1980@gmail.com>
2018-03-14 10:09:47 +0100

    eerste commit van Winst

---
+++ src/main/java/be/vdab/services/WinstService.java
@@ -0,0 +1,20 @@
+package be.vdab.services;
+
+import java.math.BigDecimal;
+
+import be.vdab.repositories.KostRepository;
+import be.vdab.repositories.OpbrengstRepository;
+
+public class WinstService {
+	private final KostRepository kostRepository;
+	private final OpbrengstRepository opbrengstRepository;
+	
+	public WinstService(KostRepository kostRepository, OpbrengstRepository opbrengstRepository) {
+		this.kostRepository = kostRepository;
+		this.opbrengstRepository = opbrengstRepository;
+	}
+	
+	public BigDecimal getWinst() {
+		throw  new UnsupportedOperationException();
+	}
+}
---
+++ src/test/java/be/vdab/repositories/KostRepositoryStub.java
@@ -0,0 +1,12 @@
+package be.vdab.repositories;
+
+import java.math.BigDecimal;
+
+public class KostRepositoryStub implements KostRepository {
+
+	public BigDecimal findTotaleKost() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
